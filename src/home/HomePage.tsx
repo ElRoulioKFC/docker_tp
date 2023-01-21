92a9c62dc2fa4bc3672b90898971056a1f63e03f
roulio <julesgeyer@outlook.fr>
2023-01-21 15:12:52 +0100

    error when change window

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -1,15 +1,17 @@
 import { Button, TextField, Typography } from "@mui/material";
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 
 export function HomePage(){
 
     return<div>
             <h1>Bonjour et bienvenue</h1>
             <p>Entrez dans notre monde de spleen en cliquant</p>
-            <Button variant="contained" onClick={() => 
-                window.location.href = "/content"
-            }>Cliquez ici</Button>
-            
+            <Link to="/content">
+                      <Button variant="contained">Création de simulation</Button>
+              </Link>
+
             
         </div>
         
